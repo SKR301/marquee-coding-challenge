610e7fcdbe4a9d0380fd239d8cc8bd26aad21447
SKR301 <skrint301@gmail.com>
2022-10-09 21:40:39 +0530

    fetched data using API

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Button, TouchableOpacity } from 'react-native-web';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Search() {
+
+	const [companies, setCompanies] = useState([]);
+
+	// const parseHTMLResp = (input) = {
+
+	// }
+
+	const inputChangeHandeler = () => {
+		axios.post('https://www.zaubacorp.com/custom-search', {search: "s", filter: "company"},{
+				"body": "search=s&filter=company",
+				"method": "POST",
+			}).then(({data}) => {
+				data = data.split(' </div>')
+				let company = []
+				data.map((line, index) => {
+					company.push(line.split('>')[1])
+				});
+				setCompanies(company);
+			});
+	}
+
+	const onSearchPressHandler = () => {
+
+	}
+
+	useEffect(()=>{
+		
+	},[companies]);
+
+	const companiesToRender = [];
+	companies.map((element, index) => {
+		companiesToRender.push(
+			<Text>{element}</Text>
+		)
+	});
+
+
 	return (
 		<View style={styles.container}>
-			<TextInput style={styles.inputFormData} placeholder={'Enter search query...'}>
+			<TextInput style={styles.inputFormData} placeholder={'Enter search query...'} onChange={inputChangeHandeler}>
 			</TextInput>
-			<TouchableOpacity style={styles.searchBtn}><Text>Search</Text></TouchableOpacity>
+			<TouchableOpacity style={styles.searchBtn} onPress={onSearchPressHandler}>
+				<Text>Search</Text>
+			</TouchableOpacity>
+			<View>
+			{
+				companiesToRender
+			}
+			</View>
 		</View>
 	);
 }
